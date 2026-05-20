fe9ba4d70fc5e8eca4b23dbe889a645e65a3bdef
srinibuson <srinivas04112@gmail.com>
2026-05-20 18:35:11 +0530

    favourite tab finish

--- lib/Pages/Dashboard/widget/favourite.dart
+++ lib/Pages/Dashboard/widget/favourite.dart
@@ -134,17 +134,6 @@ class FavouritesScreen extends StatelessWidget {
             ),
           ),
         );
-        // return GridView.builder(
-        //   padding: const EdgeInsets.all(12),
-        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        //     crossAxisCount: 2,
-        //     crossAxisSpacing: 10,
-        //     mainAxisSpacing: 10,
-        //     childAspectRatio: 0.75,
-        //   ),
-        //   itemCount: items.length,
-        //   itemBuilder: (_, i) => FavouriteCard(data: items[i]),
-        // );
       },
     );
   }
